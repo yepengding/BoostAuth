de8b5da1e42db80bc6a4f9772df296a9bd6107fa
yepengding <dingyepeng@gmail.com>
2022-08-09 17:39:01 +0900

    Fix open API base url.

--- src/main/java/org/veritasopher/boostauth/BoostAuthApplication.java
+++ src/main/java/org/veritasopher/boostauth/BoostAuthApplication.java
@@ -1,5 +1,7 @@
 package org.veritasopher.boostauth;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -9,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  *
  * @author Yepeng Ding
  */
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 @EnableJpaAuditing
 public class BoostAuthApplication {
